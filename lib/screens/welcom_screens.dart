d0fd7c1a7eefccafe66b8475dcb81c87e8ecd779
mcalisadeghi <ali.ke970@gmail.com>
2023-09-13 23:05:43 +0330

    bild HomeScreen

--- lib/screens/welcom_screens.dart
+++ lib/screens/welcom_screens.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/home_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
@@ -7,17 +9,85 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       child: Container(
-        padding: EdgeInsets.only(
+        padding: const EdgeInsets.only(
           top: 100,
           bottom: 40,
         ),
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           color: Colors.black,
           image: DecorationImage(
             image: AssetImage(
-              'assets/images/pb.png',
+              'assets/images/bg.png',
             ),
+            fit: BoxFit.cover,
+            opacity: 0.5,
           ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Coffe Shop',
+              style: GoogleFonts.pacifico(
+                fontSize: 50,
+                color: Colors.white,
+              ),
+            ),
+            Column(
+              children: [
+                Text(
+                  'Feelin Low? Take a Sip of Coffee',
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(
+                      0.8,
+                    ),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: 1,
+                  ),
+                ),
+                const SizedBox(
+                  height: 80,
+                ),
+                InkWell(
+                  splashColor: Colors.black,
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => HomeScreen(),
+                      ),
+                    );
+                  },
+                  child: Ink(
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 15,
+                        horizontal: 33,
+                      ),
+                      decoration: BoxDecoration(
+                        color: const Color(
+                          0xFFE57734,
+                        ),
+                        borderRadius: BorderRadius.circular(
+                          10,
+                        ),
+                      ),
+                      child: const Text(
+                        'Get Start',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
