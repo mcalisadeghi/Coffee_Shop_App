555965b94a886eb219329ba7b7d7e946e788dbde
mcalisadeghi <ali.ke970@gmail.com>
2023-09-14 03:05:39 +0330

    add search bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -43,10 +43,89 @@ class _HomeScreenState extends State<HomeScreen>
           top: 15,
         ),
         child: ListView(
-          children: const <Widget>[
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 15,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    child: Icon(
+                      Icons.sort_rounded,
+                      color: Colors.white.withOpacity(
+                        0.5,
+                      ),
+                      size: 35,
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Icon(
+                      Icons.notifications,
+                      color: Colors.white.withOpacity(
+                        0.5,
+                      ),
+                      size: 35,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
             Padding(
               padding: EdgeInsets.symmetric(
                 horizontal: 15,
+              ),
+              child: Text(
+                'Its a Great Day for Coffee',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 20,
+              ),
+              width: MediaQuery.of(context).size.width,
+              height: 60,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(
+                  255,
+                  50,
+                  54,
+                  56,
+                ),
+                borderRadius: BorderRadius.circular(
+                  10,
+                ),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'find youor coffee',
+                  hintStyle: TextStyle(
+                    color: Colors.white.withOpacity(
+                      0.5,
+                    ),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    size: 30,
+                    color: Colors.white.withOpacity(
+                      0.5,
+                    ),
+                  ),
+                ),
               ),
             ),
           ],
