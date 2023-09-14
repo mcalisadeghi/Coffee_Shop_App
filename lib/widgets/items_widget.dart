d68bb4d0cb7366e64eba8ed30867d3bd217ac2a2
mcalisadeghi <ali.ke970@gmail.com>
2023-09-14 17:04:04 +0330

    add imge and title box

--- lib/widgets/items_widget.dart
+++ lib/widgets/items_widget.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ItemsWidget extends StatelessWidget {
@@ -11,11 +12,123 @@ class ItemsWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return GridView.count(
       crossAxisCount: 2,
-      physics: NeverScrollableScrollPhysics(),
+      physics: const NeverScrollableScrollPhysics(),
       shrinkWrap: true,
       childAspectRatio: (150 / 195),
       children: [
-        for (int i = 0; i < img.length; i++) Container(),
+        for (int i = 0; i < img.length; i++)
+          Container(
+            padding: EdgeInsets.symmetric(
+              vertical: 5,
+              horizontal: 10,
+            ),
+            margin: EdgeInsets.symmetric(
+              vertical: 8,
+              horizontal: 13,
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(
+                20,
+              ),
+              color: Color(
+                0xFF212325,
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(
+                    0.5,
+                  ),
+                  spreadRadius: 1,
+                  blurRadius: 8,
+                ),
+              ],
+            ),
+            child: Column(
+              children: [
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    margin: EdgeInsets.all(
+                      10,
+                    ),
+                    child: Image.asset(
+                      'assets/images/${img[i]}.png',
+                      width: 120,
+                      height: 120,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    bottom: 8,
+                  ),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Column(
+                      children: [
+                        Text(
+                          img[i],
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          img[i],
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white60,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 5,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '\$30',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(
+                          5,
+                        ),
+                        decoration: BoxDecoration(
+                          color: Color(
+                            0xFFE57734,
+                          ),
+                          borderRadius: BorderRadius.circular(
+                            20,
+                          ),
+                        ),
+                        child: Icon(
+                          CupertinoIcons.add,
+                          size: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
       ],
     );
   }
